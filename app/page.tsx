281c8961701fafd5adc33c082890f40f7cb5c2f6
Ivan Wake <purplebaby16@vk.com>
2024-06-26 10:09:55 +0300

    Refactored project structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,7 @@
 const Page = () => {
     return (
         <>
-            <section id="welcome">
-                <div className="tagmate">
-                    <img src="/icons/tagmate-min.svg" width="128px" />
-                        <div className="primary">Привет!</div>
-                        <div className="secondary">
-                            TagMate - ваш проводник в мире<br/>знакомств на улице, где каждый
-                            тег<br/>становится ключом к новым друзьям<br/>и возможным свиданиям.
-                        </div>
-                </div>
-                <div className="buttons">
-                    <div className="button button-login">Войти</div>
-                    <div className="button button-register">Зарегистрироваться</div>
-                    <span>Или</span>
-                    <div className="button button-yandex">Войти через Яндекс</div>
-                </div>
-            </section>
-            <footer>
-                <div className="copyright">© 2024 «TagMate»</div>
-            </footer>
+            <h1>Profile</h1>
         </>
     );
 }
