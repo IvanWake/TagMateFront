1db1fa4c323c1d325ded3a0fafbd5bee96e7dc10
Ivan Wake <purplebaby16@vk.com>
2025-02-11 19:50:17 +0300

    Serega nu ti i utirok

--- types/userProfile/profileContent.ts
+++ types/userProfile/profileContent.ts
@@ -1,7 +1,19 @@
 import { Interest } from "@/types/signup/steps";
 
+type Social = {
+    social: string,
+    link: string
+}
+
+type Image = {
+    id: string,
+    path: string
+}
+
 export type ProfileContent = {
     tag: string
     interests: Interest[],
-    purpose: string
+    purpose: string,
+    images: Image[],
+    socials: Social[]
 }
