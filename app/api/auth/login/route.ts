439e14c4969dafe50ce334e0188ffd271406490c
Ivan Timofeev <purplebaby16@vk.com>
2025-10-22 15:14:18 +0300

    Kapec

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -2,22 +2,22 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest) {
     const body = await req.json();
+    //
+    // const res = await fetch(`${process.env.NEXT_BACKEND_URL}/api/auth/login`, {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type" : "application/json",
+    //     },
+    //     body: JSON.stringify({ tag: body.tag, password: body.password })
+    // })
+    //
+    // if (!res.ok) {
+    //     const error = await res.json();
+    //     return NextResponse.json(error, { status: res.status });
+    // }
+    //
+    // const data = await res.json();
 
-    const res = await fetch(`${process.env.NEXT_BACKEND_URL}/api/auth/login`, {
-        method: "POST",
-        headers: {
-            "Content-Type" : "application/json",
-        },
-        body: JSON.stringify({ tag: body.tag, password: body.password })
-    })
-
-    if (!res.ok) {
-        const error = await res.json();
-        return NextResponse.json(error, { status: res.status });
-    }
-
-    const data = await res.json();
-
-    return NextResponse.json(data);
+    return NextResponse.json(body.tag);
 }
 
