6ba6c3f2f6ff75938ebbfbb0b148b145c7196ab4
Ivan Timofeev <purplebaby16@vk.com>
2025-10-27 18:08:01 +0300

    Idi nahuy mates

--- components/Mates/Mates/NoMates.tsx
+++
@@ -1,9 +0,0 @@
-import mainStyles from "@/components/Mates/Mates/Mates.module.css";
-
-const NoMates = () => {
-    return (
-        <div className={mainStyles.matesText}>Возможно, ваш будущий мейт уже где-то рядом — попробуй поискать!</div>
-    );
-}
-
-export default NoMates;
