877c9dcd4ee684f158b955464ee2af9f8531567e
Ivan Timofeev <purplebaby16@vk.com>
2025-11-17 00:41:24 +0300

    DOJKhiodfabuifsdugidfguiwefruighwefhuiwefuiheuihefhui

--- app/auth/login/layout.tsx
+++ app/auth/login/layout.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import type { Metadata } from "next";
 import { Inter, Unbounded } from "next/font/google";
 import "./login.css";
+import { useEffect } from "react";
 
 const inter = Inter({
   subsets: ['cyrillic'],
@@ -14,16 +17,28 @@ const unbounded = Unbounded({
   display: 'swap',
 });
 
-export const metadata: Metadata = {
-  title: "Tagmate | Авторизация",
-  description: "TagMate - ваш проводник в мирезнакомств на улице, где каждый тег становится ключом к новым друзьям и возможным свиданиям",
-};
+// export const metadata: Metadata = {
+//   title: "Tagmate | Авторизация",
+//   description: "TagMate - ваш проводник в мирезнакомств на улице, где каждый тег становится ключом к новым друзьям и возможным свиданиям",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const body = document.body;
+      const originalClassName = body.className;
+      body.className = "EbanayaBlyadota";
+
+
+      return () => {
+        body.className = originalClassName;
+      }
+    }
+  }, []);
   return (
     <html lang="ru" className={`${inter.variable} ${unbounded.variable}`}>
       <body>{children}</body>
--- app/auth/signup/layout.tsx
+++ app/auth/signup/layout.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import type { Metadata } from "next";
 import { Inter, Unbounded } from "next/font/google";
 import "./signup.css";
+import {useEffect} from "react";
 
 
 const inter = Inter({
@@ -15,17 +18,29 @@ const unbounded = Unbounded({
   display: 'swap',
 });
 
-export const metadata: Metadata = {
-  viewport: "maximum-scale-1",
-  title: "Tagmate | Регистрация",
-  description: "TagMate - ваш проводник в мирезнакомств на улице, где каждый тег становится ключом к новым друзьям и возможным свиданиям",
-};
+// export const metadata: Metadata = {
+//   viewport: "maximum-scale-1",
+//   title: "Tagmate | Регистрация",
+//   description: "TagMate - ваш проводник в мирезнакомств на улице, где каждый тег становится ключом к новым друзьям и возможным свиданиям",
+// };
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const body = document.body;
+      const originalClassName = body.className;
+      body.className = "sukaEbanaya";
+
+
+      return () => {
+        body.className = originalClassName;
+      };
+    }
+  }, []);
   return (
     <html lang="ru" className={`${inter.variable} ${unbounded.variable}`}>
       <body>{children}</body>
