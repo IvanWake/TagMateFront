094b8ddc45abf152ec7c5160db095a46fbb8c8df
Ivan Timofeev <purplebaby16@vk.com>
2025-11-17 00:10:17 +0300

    Ya ebal kringe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import { Inter, Unbounded, Onest } from 'next/font/google';
 import indexStyles from "./index.module.css";
@@ -40,6 +40,13 @@ export const metadata: Metadata = {
     title: 'Tagmate',
   },
 };
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
