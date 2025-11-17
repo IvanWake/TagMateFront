96fe6dae9b09eb02ca70bca0d6bbbce1d1852bb8
Ivan Timofeev <purplebaby16@vk.com>
2025-11-17 16:53:43 +0300

    SOsal

--- components/Settings/ProfileEdit/ProfileEdit.tsx
+++ components/Settings/ProfileEdit/ProfileEdit.tsx
@@ -46,7 +46,7 @@ const ProfileEdit = () => {
         if (file) {
             setPhoto(URL.createObjectURL(file))
             const formData = new FormData();
-            formData.append("image", e.target.files?.[0])
+            formData.append("image", file)
             await addPhoto(formData);
             window.location.reload();
         }
