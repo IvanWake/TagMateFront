3e822b269bf5e85d4b767ed36911fb4351a0c28f
nikrasavin <nik.krasavin.06@gmail.com>
2026-02-18 18:16:21 +0300

    add feed/tag page, active state for interests and tipification for userprofile data

--- components/Interests/InterestItem.tsx
+++ components/Interests/InterestItem.tsx
@@ -1,11 +1,21 @@
 import interestStyles from "./Interests.module.css";
 
-const InterestItem = ({ name, key }: { name: string, key: number }) => {
-    return (
-        <div key={key} className={interestStyles["interest-item"]}>
-            <div className={interestStyles["interest-name"]}>{name}</div>
-        </div>
-    );
+interface InterestItemProps {
+  name: string;
+  id: number;
+  isActive?: boolean;
 }
+
+const InterestItem = ({ name, id, isActive = true }: InterestItemProps) => {
+  return (
+    <div
+      key={id}
+      className={interestStyles["interest-item"]}
+      data-active={isActive}
+    >
+      <div className={interestStyles["interest-name"]}>{name}</div>
+    </div>
+  );
+};
 
 export default InterestItem;
--- types/userProfile/profileContent.ts
+++ types/userProfile/profileContent.ts
@@ -1,45 +1,65 @@
 import { Interest } from "@/types/signup/steps";
 
 export type Social = {
-    social: string,
-    link: string
-}
+  social: string;
+  link: string;
+};
 
 export type Image = {
-    id: string,
-    path: string
-}
+  id: string;
+  path: string;
+};
 
 export type ProfileData = {
-    tag: string
-    interests: Interest[],
-    purpose: string,
-    images: Image[],
-    socials: Social[],
-    publicProfile?: boolean,
-    isUserMate: string,
-    inBlackList: boolean,
-    isBlocked: boolean
-
-}
+  tag: string;
+  interests: Interest[];
+  purpose: string;
+  images: Image[];
+  socials: Social[];
+  publicProfile?: boolean;
+  isUserMate: string;
+  inBlackList: boolean;
+  isBlocked: boolean;
+};
 
 export type PersonalData = {
-    name: string,
-    lastName: string,
-    avatar: string,
-    city: string,
-    birthDay: Date,
-    isUserByTag?: boolean,
-    tag: string,
-    isUserMate: string,
-    isBlocked: boolean,
-    inBlackList: boolean
-}
+  name: string;
+  lastName: string;
+  avatar: string;
+  city: string;
+  birthDay: Date;
+  isUserByTag?: boolean;
+  tag: string;
+  isUserMate: string;
+  isBlocked: boolean;
+  inBlackList: boolean;
+};
 
 export type BannerData = {
-    link: string,
-    image: string,
-    title: string,
-    description: string,
-    buttonText: string
+  link: string;
+  image: string;
+  title: string;
+  description: string;
+  buttonText: string;
+};
+
+export interface UserProfileResponse {
+  userData: {
+    name: string;
+    lastName: string;
+    city: {
+      city: string;
+    };
+    avatar: {
+      path: string;
+    };
+    birthDay: Date;
+    serviceId: string;
+    purpose: string;
+    interests: Interest[];
+    socials: Social[];
+    images: Image[];
+  };
+  isBlocked: boolean;
+  inBlackList: boolean;
 }
